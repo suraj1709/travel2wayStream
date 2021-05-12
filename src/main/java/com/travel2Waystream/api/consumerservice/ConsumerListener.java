0cbb90ed333d13a760af9b3e2ea2ec8265b8999e
suraj <surajdey8603@gmail.com>
2021-05-12 20:37:18 +0530

    Two consumer group

--- src/main/java/com/travel2Waystream/api/consumerservice/ConsumerListener.java
+++ src/main/java/com/travel2Waystream/api/consumerservice/ConsumerListener.java
@@ -4,5 +4,7 @@ import com.travel2Waystream.api.model.Place;
 
 public interface ConsumerListener {
 
-    public void consume(Place place);
+    public void consume1(Place place);
+
+    public void consume2(Place place);
 }
